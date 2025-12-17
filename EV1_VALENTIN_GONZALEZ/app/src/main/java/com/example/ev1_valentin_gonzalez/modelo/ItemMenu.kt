ab77c6333bc4d9731641e62332698fdf32e32ec8
Jahy <237892348+Jahycode@users.noreply.github.com>
2025-12-16 22:46:24 -0300

    Delete EV1_VALENTIN_GONZALEZ directory

--- EV1_VALENTIN_GONZALEZ/app/src/main/java/com/example/ev1_valentin_gonzalez/modelo/ItemMenu.kt
+++
@@ -1,5 +0,0 @@
-package com.example.ev1_valentin_gonzalez.modelo
-
-class ItemMenu(val nombre:String,val precio:Int) {
-
-}
--- EV1_VALENTIN_GONZALEZ/app/src/main/java/com/example/ev1_valentin_gonzalez/modelo/ItemMesa.kt
+++
@@ -1,8 +0,0 @@
-package com.example.ev1_valentin_gonzalez.modelo
-
-class ItemMesa(val ItemMenu: ItemMenu,val cantidad:Int) {
-
-    fun calcularSubtotal(): Int{
-        return ItemMenu.precio * cantidad
-    }
-}
